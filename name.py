888c858fcd10cd12893a6ba998d01e187088ecb5
Chayabhorn <93228207+Chayabhorn@users.noreply.github.com>
2021-10-27 05:02:18 +0700

    Add files via upload

---
+++ name.py
@@ -0,0 +1,12 @@
+import numpy
+import scipy
+import matplotlib
+import sklearn # scikit-learn library
+import skimage # scikit-image library
+import h5py
+import pandas
+import keras
+import tensorflow
+import cv2 # opencv library
+str = 'hello word'
+print(str)
